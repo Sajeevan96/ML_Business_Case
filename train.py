9c5eb9f88e0414a23c32c8afa7c932fa333741bf
Alchl <alann.cheral@hotmail.fr>
2019-02-13 21:43:09 +0100

    commentaires train

--- train.py
+++ train.py
@@ -15,19 +15,29 @@ def feature_extractor(X):
         Extract some new features from the dataset. 
         X: Dataset (without labels)
     '''
+    # add some distincts columns for Month, Year and Day 
     X['Month'] = X.Date.dt.month
     X['Year'] = X.Date.dt.year
     X['Day'] = X.Date.dt.day
+    
+    # return a number indicating which week of the year the date falls in
     X['Week'] = X.Date.dt.weekofyear
+    
+    # remove the Date's column 
     X = X.drop('Date', 1)
     
+    # calculate monthly/weekly/daily average customers
     X['AVG_Month'] = X[['Year','Month','Customers']].groupby(['Year','Month']).transform('mean')
     X['AVG_Week'] = X[['Year','Week','Customers']].groupby(['Year','Week']).transform('mean')
     X['AVG_Day'] = X[['Year','DayOfWeek','Customers']].groupby(['Year','DayOfWeek']).transform('mean')
 
+    # calculate how long the nearest competitor has been opened
     X['Competition_Duration'] = 12 *(X.Year - X.CompetitionOpenSinceYear) *(X.CompetitionOpenSinceYear>0) + (X.Month - X.CompetitionOpenSinceMonth) *(X.CompetitionOpenSinceMonth>0)
     
+    # calculate the duration of Promo2
     X['Promo_Duration'] = 12 *(X.Year - X.Promo2SinceYear) *(X.Promo2SinceYear>0) + (X.Week - X.Promo2SinceWeek) *(X.Promo2SinceWeek>0)
+    
+    # remove some columns 
     X = X.drop('CompetitionOpenSinceYear', 1)
     X = X.drop('CompetitionOpenSinceMonth', 1)
     X = X.drop('Promo2SinceYear', 1)
